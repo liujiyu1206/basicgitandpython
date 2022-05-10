12ab5b4763af49461ea4fca1d747b384f0d6e14a
JiyuLiu202256 <jliu234@sheffield.ac.uk>
2022-05-10 14:30:01 +0100

    20220510_1429

--- example.py
+++ example.py
@@ -5,9 +5,13 @@ from PySide6.QtCore import QSize, Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
+ 
 
 class MainWindow(QMainWindow):
     def __init__(self):
+# When you subclass a Qt class 
+# you must always call the super __init__ function
+# to allow Qt to set up the object
         super().__init__()
 
 
